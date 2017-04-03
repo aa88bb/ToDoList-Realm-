31f371a9599fd0e58a1e15b514981428ccf647bc
aa88bb <zhuchenglong123@gmail.com>
2017-04-02 20:07:39 -0400

    finished tasklist

--- ToDoList_Realm/ToDoList_Realm/TaskListsVC.swift
+++ ToDoList_Realm/ToDoList_Realm/TaskListsVC.swift
@@ -15,7 +15,7 @@ class TaskListsVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     let myRealm = try! Realm()
     var lists:Results<TaskList>!
     var isEditingMode = false
-    
+    var currentCreateAction:UIAlertAction!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,12 +30,14 @@ class TaskListsVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
     }
     
     @IBAction func editAction(_ sender: UIBarButtonItem) {
-        
+        isEditingMode = !isEditingMode
+        taskListTableView.setEditing(isEditingMode, animated: true)
+        taskListTableView.reloadData()
     }
     
     
     @IBAction func addAction(_ sender: UIBarButtonItem) {
-        
+        displayAlertToAddTaskList(nil)
     }
     
 
@@ -71,6 +73,74 @@ class TaskListsVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
         cell.detailTextLabel?.text = "\(list.tasks.count) tasks."
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "Delete") { (action, indexpath) in
+            let listToBeDeleted = self.lists[indexpath.row]
+            
+            try! self.myRealm.write {
+                self.myRealm.delete(listToBeDeleted)
+                self.taskListTableView.reloadData()
+            }
+        }
+        
+        let editAction = UITableViewRowAction(style: .normal, title: "Edit") { (action, indexpath) in
+            let listToBeEdit = self.lists[indexpath.row]
+            self.displayAlertToAddTaskList(listToBeEdit)
+        }
+        return [deleteAction,editAction]
+    }
+    
+    
+    
+    func displayAlertToAddTaskList(_ updatedList:TaskList!){
+        var title = "New Task List"
+        var doneTitle = "Create"
+        
+        if updatedList != nil {
+            title = "Update Task List"
+            doneTitle = "Update"
+        }
+        
+        let alertController = UIAlertController(title: title, message: "write the name of your task list.", preferredStyle: .alert)
+        let createAction = UIAlertAction(title: doneTitle, style: .default) { (action) in
+            let listName = alertController.textFields?.first?.text
+            if updatedList != nil {
+                try! self.myRealm.write {
+                    updatedList.name = listName!
+                    self.taskListTableView.reloadData()
+                }
+            }else{
+                let newTaskList = TaskList()
+                newTaskList.name = listName!
+                try! self.myRealm.write {
+                    self.myRealm.add(newTaskList)
+                    self.taskListTableView.reloadData()
+                }
+            }
+        }
+        alertController.addAction(createAction)
+        createAction.isEnabled = false
+        self.currentCreateAction = createAction
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        alertController.addTextField { (textfield) in
+            textfield.placeholder = "Task List Name"
+            textfield.addTarget(self, action: #selector(self.listNameFieldDidChange(_:)), for: UIControlEvents.editingChanged)
+            if updatedList != nil {
+                textfield.text = updatedList.name
+            }
+        }
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func listNameFieldDidChange(_ textField:UITextField){
+        self.currentCreateAction.isEnabled = (textField.text?.characters.count)! > 0
+    }
+    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "openTasks", sender: self.lists[indexPath.row])
